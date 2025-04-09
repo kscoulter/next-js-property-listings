f6def5319f6f7a8322221b0e2d3e43d89af51d95
Ksenia Coulter <kseniacoulter@gmail.com>
2025-04-09 12:32:02 -0400

    add a custom 404 pages

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,9 +1,11 @@
 import properties from '@/properties.json'
 import PropertyCard from './PropertyCard';
+import Link from 'next/link';
 
 const HomeProperties = () => {
   const recentProperties = properties.slice(0,3);
-    return (
+  return (
+    <>
       <section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto px-4 py-6'>
           <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
@@ -19,8 +21,12 @@ const HomeProperties = () => {
             </div>
           )}
         </div>
-      </section>  
-    );
+      </section>
+      <section className='m-auto max-w-lg my-6 px-6'>
+        <Link href='/properties' className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'>View All Properties</Link>
+      </section>
+    </>
+  );
 }
  
 export default HomeProperties;
